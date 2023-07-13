a05e8d6af37a46bf9772b8df550a2833e27d1bff
Takeo Kondo <takeoverjp@gmail.com>
2023-07-13 22:56:08 +0900

    feat: support application-id

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,11 @@ fn cli() -> Command {
                 )
                 .arg_required_else_help(true)
                 .arg(
+                    arg!(-a --"application-id" <APPLICATION_ID>)
+                        .value_parser(clap::builder::NonEmptyStringValueParser::new())
+                        .default_value("DLTC"),
+                )
+                .arg(
                     arg!(-c --"context-id" <CONTEXT_ID>)
                         .value_parser(clap::builder::NonEmptyStringValueParser::new())
                         .default_value("DLTC"),
@@ -30,39 +35,48 @@ fn cli() -> Command {
         )
 }
 
+fn dlt_log(apid: &str, ctid: &str, msg: &str) {
+    let app_name = CString::new(apid).unwrap();
+    unsafe {
+        dlt_register_app(
+            app_name.as_ptr(),
+            CString::new("Example Application").unwrap().as_ptr(),
+        );
+    }
+    let mut context = DltContext::new_uninitialized();
+    let context_id = CString::new(ctid).unwrap();
+    let description = CString::new("This is a longer description").unwrap();
+    unsafe {
+        dlt_register_context(
+            context.as_mut_ptr(),
+            context_id.as_ptr(),
+            description.as_ptr(),
+        );
+    }
+    let mut local_context = DltContextData::new_uninitialized();
+    let message = CString::new(msg.to_string()).unwrap();
+
+    unsafe {
+        let _dlt_local = dlt_user_log_write_start_id(
+            context.as_mut_ptr(),
+            local_context.as_mut_ptr(),
+            DltLogLevelType::DLT_LOG_ERROR,
+            1234,
+        );
+        dlt_user_log_write_string(local_context.as_mut_ptr(), message.as_ptr());
+        dlt_user_log_write_finish(local_context.as_mut_ptr());
+    }
+}
+
 fn main() {
     let matches = cli().get_matches();
 
     match matches.subcommand() {
         Some(("log", sub_matches)) => {
-            let msg: &String = sub_matches.get_one("MESSAGE").expect("required");
+            let apid: &String = sub_matches.get_one("application-id").expect("required");
             let ctid: &String = sub_matches.get_one("context-id").expect("required");
-            let message = CString::new(msg.to_string()).unwrap();
-            unsafe {
-                let app_name = CString::new("APP").unwrap();
-                dlt_register_app(
-                    app_name.as_ptr(),
-                    CString::new("Example Application").unwrap().as_ptr(),
-                );
-                let mut context = DltContext::new_uninitialized();
-                let context_id = CString::new(ctid.clone()).unwrap();
-                let description = CString::new("This is a longer description").unwrap();
-                dlt_register_context(
-                    context.as_mut_ptr(),
-                    context_id.as_ptr(),
-                    description.as_ptr(),
-                );
-                let mut local_context = DltContextData::new_uninitialized();
-
-                let _dlt_local = dlt_user_log_write_start_id(
-                    context.as_mut_ptr(),
-                    local_context.as_mut_ptr(),
-                    DltLogLevelType::DLT_LOG_ERROR,
-                    1234,
-                );
-                dlt_user_log_write_string(local_context.as_mut_ptr(), message.as_ptr());
-                dlt_user_log_write_finish(local_context.as_mut_ptr());
-            }
+            let msg: &String = sub_matches.get_one("MESSAGE").expect("required");
+            dlt_log(apid, ctid, msg);
         }
         Some((ext, sub_matches)) => {
             let args = sub_matches
